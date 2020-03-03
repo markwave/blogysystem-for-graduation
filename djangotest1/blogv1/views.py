5e8984ef2ae5fac399a6c01b6a6f1d034a285c0d
June <468204510@qq.com>
2020-03-03 20:14:18 +0800

    分页器完成

--- djangotest1/blogv1/views.py
+++ djangotest1/blogv1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404
 from .models import blogv1,BlogType
 from django.core.paginator import Paginator
+from django.conf import settings
 
 # Create your views here.
 def article_detail(request,b_id):
@@ -13,22 +14,52 @@ def article_detail(request,b_id):
 
 def article_list(request):
 	articles=blogv1.objects.filter(is_deleted=False)
-	paginator=Paginator(articles,10) #每10篇进行分页
+	paginator=Paginator(articles,settings.EACH_PAGE_BLOGS_NUMBER) #每10篇进行分页
 	page_num=request.GET.get('page',1) #获取url学生页面参数 
 	page_of_blogs=paginator.get_page(page_num)
+	currentr_page_num=page_of_blogs.number#获取当前页码
+	page_range=list(range(max(currentr_page_num-2,1),currentr_page_num))+\
+			   list(range(currentr_page_num,min(currentr_page_num+2,paginator.num_pages)+1))
+	if page_range[0]-1>=2:
+		page_range.insert(0,'...')
+	if paginator.num_pages-page_range[-1]>=2:
+		page_range.append('...')
+	if page_range[0]!=1:
+		page_range.insert(0,1)
+	if page_range[-1]!=paginator.num_pages:
+		page_range.append(paginator.num_pages)
 
 	context={}
 	context['blogs']=page_of_blogs.object_list
 	context['blog']=page_of_blogs
+	context['page_range']=page_range
 	context['blog_types']=BlogType.objects.all( )
 	return render(request,"blog/article_list.html",context)
 
 	
 def blogs_with_type(request, blog_type_pk):
-    context = {}
+    
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    context['blogs'] = blogv1.objects.filter(blog_type=blog_type)
+    articles=blogv1.objects.filter(blog_type=blog_type)
+    paginator=Paginator(articles,settings.EACH_PAGE_BLOGS_NUMBER) #每10篇进行分页
+    page_num=request.GET.get('page',1)
+    page_of_blogs=paginator.get_page(page_num)
+    currentr_page_num=page_of_blogs.number#获取当前页码
+    page_range=list(range(max(currentr_page_num-2,1),currentr_page_num))+\
+               list(range(currentr_page_num,min(currentr_page_num+2,paginator.num_pages)+1))
+    if page_range[0]-1>=2:
+        page_range.insert(0,'...')
+    if paginator.num_pages-page_range[-1]>=2:
+        page_range.append('...')
+    if page_range[0]!=1:
+        page_range.insert(0,1)
+    if page_range[-1]!=paginator.num_pages:
+        page_range.append(paginator.num_pages)
+    context={}
+    context['blogs']=page_of_blogs.object_list
+    context['blog']=page_of_blogs
     context['blog_type'] = blog_type
+    context['page_range']=page_range
     context['blog_types']=BlogType.objects.all( )
     return render(request,'blog/blogs_with_type.html', context)
 	
--- djangotest1/djangotest1/settings.py
+++ djangotest1/djangotest1/settings.py
@@ -124,3 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),
     ]
+
+
+#自定义参数
+EACH_PAGE_BLOGS_NUMBER=7
