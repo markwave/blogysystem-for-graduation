91b8c6d8b3865e9b75af0fa3e9ab3a7f2d638b6f
June <468204510@qq.com>
2020-03-05 13:32:45 +0800

    阅读数日期模型及图表显示

--- djangotest1/blogv1/models.py
+++ djangotest1/blogv1/models.py
@@ -20,7 +20,7 @@ class blogv1(models.Model,ReadNumExpandMethod):
 	last_updated_time=models.DateTimeField(auto_now=True)
 	author=models.ForeignKey(User,on_delete=models.DO_NOTHING,default=1) 
 	is_deleted=models.BooleanField(default=False)
-	read_num= models.IntegerField(default=0)
+	
 	blog_type=models.ForeignKey(BlogType,on_delete=models.DO_NOTHING,default=1)
 	def __str__(self):
 		return "Article:%s" % self.title
--- djangotest1/djangotest1/views.py
+++ djangotest1/djangotest1/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
 
+from django.shortcuts import render_to_response
+from django.contrib.contenttypes.models import ContentType
+from read_statistics.utils import get_seven_days_read_data
+from blogv1.models import blogv1
 
 def home(request):
-	return render(request,"blog/home.html")
+    blog_content_type = ContentType.objects.get_for_model(blogv1)
+    dates, read_nums = get_seven_days_read_data(blog_content_type)
+
+    context = {}
+    context['dates'] = dates
+    context['read_nums'] = read_nums
+    return render_to_response('blog/home.html', context)
