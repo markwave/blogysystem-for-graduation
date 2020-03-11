90e5b9807a7fe4f16dc42e50e9bd69f3f667d4be
June <468204510@qq.com>
2020-03-11 21:27:14 +0800

    邮件通知评论

--- djangotest1/blogv1/models.py
+++ djangotest1/blogv1/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.contrib.contenttypes.fields import GenericRelation
 from read_statistics.models import ReadNumExpandMethod,ReadDetail
+from django.urls import reverse
 # Create your models here.
 class BlogType(models.Model):
 	type_name=models.CharField(max_length=15)
@@ -23,6 +24,11 @@ class blogv1(models.Model,ReadNumExpandMethod):
 	is_deleted=models.BooleanField(default=False)
 	read_details = GenericRelation(ReadDetail)
 	blog_type=models.ForeignKey(BlogType,on_delete=models.CASCADE,default=1)
+	def get_url(self):
+		return reverse('article_detail', kwargs={'b_id': self.pk})
+
+	def get_email(self):
+		return self.author.email
 	def __str__(self):
 		return "Article:%s" % self.title
 
